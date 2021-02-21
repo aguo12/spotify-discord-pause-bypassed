9fd68fdcd1e765038a7c74fec3f50644e525f8db
Andy Guo <info@aguo.dev>
2021-02-21 13:28:19 -0500

    poggy

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-!function(t){var e=XMLHttpRequest.prototype,o=e.send,s=e.open;e.open=function(){this._method=arguments[0],this._url=arguments[1],s.apply(this,arguments)},e.send=function(){"put"===this._method.toLowerCase()&&"https://api.spotify.com/v1/me/player/pause"===this._url&&this.abort(),console.log("Req intercepted. Follow @andyguo12"),o.apply(this,arguments)}}();
 ! function(t) {
     var e = XMLHttpRequest.prototype,
         o = e.send,
@@ -6,6 +5,6 @@
     e.open = function() {
         this._method = arguments[0], this._url = arguments[1], s.apply(this, arguments)
     }, e.send = function() {
-        "put" === this._method.toLowerCase() && "https://api.spotify.com/v1/me/player/pause" === this._url && this.abort(), console.log("Req intercepted. Follow @andyguo12"), o.apply(this, arguments)
+        "put" === this._method.toLowerCase() && "https://api.spotify.com/v1/me/player/pause" === this._url && this.abort(), console.log("Spotify pause bypassed"), o.apply(this, arguments)
     }
 }();
