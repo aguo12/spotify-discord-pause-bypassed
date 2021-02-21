1729442beba810028ad306fce4bd9811d41e122c
Andy Guo <info@aguo.dev>
2021-02-21 10:29:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+!function(t){var e=XMLHttpRequest.prototype,o=e.send,s=e.open;e.open=function(){this._method=arguments[0],this._url=arguments[1],s.apply(this,arguments)},e.send=function(){"put"===this._method.toLowerCase()&&"https://api.spotify.com/v1/me/player/pause"===this._url&&this.abort(),console.log("Req intercepted. Follow @andyguo12"),o.apply(this,arguments)}}();
